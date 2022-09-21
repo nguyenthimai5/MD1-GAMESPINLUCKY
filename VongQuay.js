d76b5c4ebf14092559cd6063ffb613e4fb6e11c4
nguyenthimai5 <mikhailthv@gmail.com>
2022-09-21 11:21:34 +0700

    SPINLUCKY

---
+++ VongQuay.js
@@ -0,0 +1,13 @@
+
+
+    // setTimeout(function (){
+    //     Swal.fire({
+    //         title: 'Sweet!',
+    //         text: 'Modal with a custom image.',
+    //         imageUrl: 'https://thegioiloaicho.com/wp-content/uploads/2021/03/yeontan-cun-cung-cua-v-bts2.jpg',
+    //         imageWidth: 400,
+    //         imageHeight: 200,
+    //         imageAlt: 'Custom image',
+    //     })
+    // },10500)
+
